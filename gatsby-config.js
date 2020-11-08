efcaa876c2aef36eef6de5e22d0e84452930c184
Muhammad Syukri <39161232+syukrie77@users.noreply.github.com>
2020-11-08 16:23:13 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Bantani Kopi',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Bantani Kopi, Kopinya Asli Indonesia.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
